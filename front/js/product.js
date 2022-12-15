79ba7abe5bb7e953d4609413de26c31c156e1a81
Effix <107648239+Effix1@users.noreply.github.com>
2022-12-15 23:06:02 +0100

    debut page product

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,38 @@
-var str = "https://waytolearnx.com/t.html?name=alex-babtise&age=25&address=paris";
-var url = new URL(str);
-var nom = url.searchParams.get("name");
-console.log(nom);                                                                                                              
+let pageProduitUrl = window.location.href
+let url = new URL(pageProduitUrl);
+const id = url.searchParams.get('id')
+console.log(id)
+let image = document.createElement('img')
+
+let itemImage = document.getElementById('item__img')
+fetch("http://localhost:3000/api/products")
+.then(function(res){
+    if (res.ok) {
+        return res.json();
+        
+    }    
+})    
+.then(function(products){
+    
+    console.log(products[1]) 
+    products.forEach(produit => {
+        let productId = produit.id;
+        for ( let i in produit.id){
+            console.log(i)
+        }
+        console.log(produit._id)
+        if (id==productId) {
+            image.setAttribute('src', produit.imageUrl)
+            itemImage.appendChild(image)
+        }
+    });                                                                            
+    
+    
+    
+    
+    return products
+}
+
+
+)    
+
