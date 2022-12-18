ede92338b230bf2b0c5c02de295d15437106ef06
Effix <107648239+Effix1@users.noreply.github.com>
2022-12-18 16:10:43 +0100

    integration des element descriptif, prix, nom, sur la page products

--- front/js/product.js
+++ front/js/product.js
@@ -13,6 +13,9 @@ fetch("http://localhost:3000/api/products")
 })    
 .then(function(products){
     const description = document.getElementById('description')
+    const title = document.getElementById('title')
+    const price = document.getElementById('price')
+
     const itemImg = document.getElementsByClassName('item__img')
     const firstItem = itemImg[0];
     const image = document.createElement('img')
@@ -23,6 +26,8 @@ fetch("http://localhost:3000/api/products")
         if (produit._id==id){
             console.log(produit);
             description.innerText = produit.description
+            title.innerText = produit.name
+            price.innerText = produit.price
             firstItem.appendChild (image)
             image.setAttribute('src', produit.imageUrl)
             console.log(firstItem)
