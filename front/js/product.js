6ffb38fe757ea87c2fae321af72789bc515804ef
Effix <107648239+Effix1@users.noreply.github.com>
2022-12-22 00:09:32 +0100

    1er essaie insertion localStorage

--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,5 @@
-let pageProduitUrl = window.location.href;
-let url = new URL(pageProduitUrl);
+let url = new URL(window.location.href);
 const id = url.searchParams.get('id');
-
-
-
 
 fetch("http://localhost:3000/api/products")
 .then(function(res){
@@ -16,55 +12,85 @@ fetch("http://localhost:3000/api/products")
     const title = document.getElementById('title')
     const price = document.getElementById('price')
 
-   
-
-    
     for (let produit of products){
         
         const itemImg = document.getElementsByClassName('item__img')
         const firstItem = itemImg[0];
         const image = document.createElement('img')
+
         const creaImage = () => {
             image.setAttribute('src', produit.imageUrl)
             image.setAttribute('alt',produit.altTxt)
             firstItem.appendChild (image)
-            console.log(firstItem)
+           // console.log(firstItem)
+        }
+        
+        const choixCouleur = () => {
+            for(let couleur of produit.colors){
+                let colors = document.getElementById('colors')
+                let option = document.createElement('option')
+                colors.appendChild(option)
+                option.innerText = couleur
+
+
+            }    
         }
 
         const creaItem = () => {
             if (produit._id==id){
-                console.log(produit);
-                
+                //console.log(produit);
+                choixCouleur();
                 creaImage();
                 description.innerText = produit.description
                 title.innerText = produit.name
                 price.innerText = produit.price
-                
-                let couleurs = produit.colors;
-                for(let couleur of couleurs){
-                    let colors = document.getElementById('colors')
-                    let option = document.createElement('option')
-                    colors.appendChild(option)
-                    option.innerText = couleur
-
-
-                }
-                
-
-                    
-                ;
-                
+                //console.log(price)
+                     
             }else {
             }
         }
+        const soloItem = creaItem()
 
-        
-        //colors();
-        const soloItem = creaItem();
 
-     
-        
    }
+
+   //     ****************       écoute du produit    *********************
+   const cart = document.getElementById('addToCart')
+   cart.addEventListener("click", (e)=>{
+       const quantity = document.querySelector('#quantity').value
+       const colors = document.querySelector('#colors').value
+       if (colors == null || colors=="" && quantity == 0){
+           alert('veuillez choisir une couleur et une quantité SVP ')
+        }else if (quantity == 0)  {
+            alert('veuillez choisir une quantity SVP')
+        }else if (colors == ""){
+            alert('veuillez choisir une couleur SVP')
+        }
+        else {}
+        localStorage.setItem(id)
+       
+       
+   } )
+
+
+
+
+   //        *****************       array du produit *****************
+   const commande = {
+    id: id,
+    //name: produit.name,
+    
+}
+//console.log(commande)
+//                                mise en local storage                            //
+//console.log (document.getElementById("colors").value);
+//const quantity = (document.getElementById("quantity").value);
+//console.log (quantity)
+localStorage.setItem = (id, colors, quantity)
+//localStorage.countProduct(("quantity").value)
+//const quantite = document.querySelector('#itemQuantity').value;
+//console.log(quantite)
+
 }
 )    
 
--- front/js/script.js
+++ front/js/script.js
@@ -8,7 +8,7 @@ fetch("http://localhost:3000/api/products")
     .then(function(products){
         
        products.forEach(produit => {
-
+////////////////////////////////constante/////////////////////////////////////////
         const items = document.getElementById('items')
         const link = document.createElement('a');
         link.href = "./product.html?id=" + produit._id
@@ -16,20 +16,20 @@ fetch("http://localhost:3000/api/products")
         const image = document.createElement('img');
         const h3 = document.createElement('h3');
         const paragraph = document.createElement('p')
-        
+//                          fonction création de l'image du produit       
         const creationImage = () => {
             image.setAttribute('src', produit.imageUrl)
             image.setAttribute('alt', produit.altTxt)
         }
-        
+//                          fonction nom du produit
         const creationH3 = () => {
             h3.innerText = (produit.name)
         }
-        
+//                          fonction de description du produit  
         const creationParagraph = () => {
             paragraph.innerText = (produit.description)
         }
-        
+//                          fonction de création de l'article        
         const creationArticle = () => {
         items.appendChild(link)
         link.appendChild(article)
