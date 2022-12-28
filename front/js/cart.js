896d18e6dbafe9c2a5b9682a733f939c1dc0e125
Effix <107648239+Effix1@users.noreply.github.com>
2022-12-28 23:39:56 +0100

    nom + prix + image sur page cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,25 +1,47 @@
-fetch("http://localhost:3000/api/products")
-    .then(function(res){
-        if (res.ok) {
-            return res.json();
-            
-        }
-    })
-    .then(function(products) {
-        const cart = JSON.parse (localStorage.getItem('caddy'))|| []
-        console.log(cart )
-        //console.log(cart[2].color )
-        //console.log(cart [2].id)
-        //console.log(cart [2].quantity)
-        console.log(products)
-        products.forEach(produit => {
-            console.log(produit._id)
-            if(cart.id=produit._id){
-                  console.log('yes')
-              }else{
-                  console.log('no')
-              }
-            
-        });
-    })
+const cart = JSON.parse (localStorage.getItem('caddy'))|| []
+cart.forEach(panier => {
+   
+    const creationArticle = function (){
+        const cartItems = document.getElementById('cart__items') ;
+        const div = document.createElement('div');
+        const article = document.createElement('article');
+        cartItems.appendChild(article);
+        article.appendChild(div);
+        div.classList.add("cart__item__img");
+        article.classList.add("cart__item");
+       
+        const image = document.createElement('img');
+        image.setAttribute('src', panier.image);
+        div.appendChild(image);
+        article.appendChild(document.createElement('div'));
+        
+        
+        const cartItemContent = (article.children[1]);
+        
+        cartItemContent.classList.add("cart__item__content");
+        cartItemContent.appendChild(document.createElement('div')).classList.add('cart__item__content__settings')
+        const contentSettings = ( cartItemContent.children[0])
+            .appendChild(document.createElement('h2'));
+        contentSettings.innerText = (panier.nom);
+        const divContentSettings = (document.getElementsByClassName('cart__item__content__settings'))
+        const para1 = document.createElement('p');
+        const para2 = document.createElement('p')
+        const paragraphe1 = contentSettings.insertAdjacentElement("afterend",(para1))
+            .insertAdjacentElement("afterend", (para2));
+
+        
+        para1.innerText= (panier.colors)
+        
+        let prix = (panier.prix + ',00 €')
+        para2.innerText = (prix)
+        
+        
+        
+    
+        
+    }
+    creationArticle();   
+});
+
+
 
--- front/js/product.js
+++ front/js/product.js
@@ -8,72 +8,78 @@ fetch("http://localhost:3000/api/products/"+ id)
     }    
 })    
 .then(function(produit){
-    console.log(produit)
 
+    const nom = (produit.name)
+    const imageProduit = (produit.imageUrl)
+    const textAlt = (produit.imageUrl)
+    const prixUnitaire= (produit.price)
     const description = document.getElementById('description')
     const title = document.getElementById('title')
     const price = document.getElementById('price')
-
     
+    const itemImg = document.getElementsByClassName('item__img')
+    const firstItem = itemImg[0];
+    const image = document.createElement('img')
+    
+    const creaImage = () => {
+        image.setAttribute('src', produit.imageUrl)
+        image.setAttribute('alt',produit.altTxt)
+        firstItem.appendChild (image)
+    }
+    
+    
+    
+    const choixCouleur = () => {
+        for(let couleur of produit.colors){
+            let colors = document.getElementById('colors')
+            let option = document.createElement('option')
+            colors.appendChild(option)
+            option.innerText = couleur
+        }    
+    }
+    
+    const creaItem = () => {
         
-        const itemImg = document.getElementsByClassName('item__img')
-        const firstItem = itemImg[0];
-        const image = document.createElement('img')
-
-        const creaImage = () => {
-            image.setAttribute('src', produit.imageUrl)
-            image.setAttribute('alt',produit.altTxt)
-            firstItem.appendChild (image)
-           // console.log(firstItem)
-        }
-        
-        const choixCouleur = () => {
-            for(let couleur of produit.colors){
-                let colors = document.getElementById('colors')
-                let option = document.createElement('option')
-                colors.appendChild(option)
-                option.innerText = couleur
-
-
-            }    
-        }
-
-        const creaItem = () => {
+        choixCouleur();
+        creaImage();
+        description.innerText = produit.description
+        title.innerText = produit.name
+        price.innerText = produit.price
+        const addToCart = document.getElementById('addToCart')
+  
+        addToCart.addEventListener("click", (e)=>{
+            const quantity = parseInt(document.querySelector('#quantity').value) 
             
-                //console.log(produit);
-                choixCouleur();
-                creaImage();
-                description.innerText = produit.description
-                title.innerText = produit.name
-                price.innerText = produit.price
-                //console.log(price)
-                const addToCart = document.getElementById('addToCart')
-                addToCart.addEventListener("click", (e)=>{
-                    const quantity = parseInt(document.querySelector('#quantity').value) 
-                    
-                    const colors = document.querySelector('#colors').value
-                    if ( colors=="" && quantity == 0){
-                        alert('veuillez choisir une couleur et une quantité SVP ')
-                     }else if (quantity == 0)  {
-                         alert('veuillez choisir une quantity SVP')
-                     }else if (colors == ""){
-                         alert('veuillez choisir une couleur SVP')
-                     }
-                     else {
-                         //alert('Vous avez choisi ' + quantity + " "+produit.name + "" + colors)
-                        const cart = JSON.parse (localStorage.getItem('caddy'))|| []
-                        console.log(cart)
-                        const identifiantExistant =cart.findIndex(item =>item.id == id && item.color == colors)
-                        console.log(identifiantExistant)
-                        if (identifiantExistant==-1) {
-                            const cartItem = {
-                                id:id,
-                                color:colors,
-                                quantity:quantity,
-                            }
-                            cart.push(cartItem)
-                        }else {
-                            cart[identifiantExistant].quantity+=quantity
+            const colors = document.querySelector('#colors').value
+            if ( colors=="" && quantity == 0){
+                alert('veuillez choisir une couleur et une quantité SVP ')
+            }else if (quantity == 0)  {
+                alert('veuillez choisir une quantity SVP')
+            }else if (colors == ""){
+                alert('veuillez choisir une couleur SVP')
+            }
+            else {
+                alert('Vous avez choisi ' + quantity + " "+produit.name + "" + colors)
+                const cart = JSON.parse (localStorage.getItem('caddy'))|| []
+                console.log(cart)
+                const identifiantExistant =cart.findIndex(item =>item.id == id && item.color == colors)
+                
+                if (identifiantExistant==-1) {
+                    const cartItem = {
+                        id:id,
+                        color:colors,
+                        quantity:quantity,
+                        image:imageProduit,
+                        description:textAlt,
+                        nom:nom,
+                        prix:prixUnitaire,
+                        
+                    }
+                    console.log(prixUnitaire)
+                    console.log(cartItem)
+                    cart.push(cartItem)
+                }else {
+                    cart[identifiantExistant].quantity+=quantity
                         }
                         
                         localStorage.setItem('caddy', JSON.stringify(cart))
