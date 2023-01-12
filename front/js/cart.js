55eeee5383138606c0575c2894f3e1df89aeb2bd
Effix <107648239+Effix1@users.noreply.github.com>
2023-01-12 18:57:23 +0100

    ajout fonction total du prix

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,5 @@
 const cart = JSON.parse(localStorage.getItem('caddy'))||[] 
-
+let somme = 0
 cart.forEach(panier => {
     fetch('http://localhost:3000/api/products/'+panier.id)
     .then (function(res){
@@ -9,30 +9,72 @@ cart.forEach(panier => {
 
     })
     .then (function(produit){
-        const sectionItem = document.getElementById('cart__items')
-        const article = creaArticle(sectionItem, "cart__item")
+        const sectionItem = document.getElementById('cart__items');
+        const article = creaArticle(sectionItem, "cart__item");
         const divImage = creaDiv(article, "cart__item__img"); 
         creaImg(divImage, produit.imageUrl);
         const divContent = creaDiv(article, "cart__item__content");
-        const divDescription = creaDiv(divContent, "cart__item__content__description")
+        const divDescription = creaDiv(divContent, "cart__item__content__description");
         creaH2(divDescription, produit.name);
-        creaParagraph(divDescription, panier.color)
+        creaParagraph(divDescription, panier.color);
         creaParagraph(divDescription, produit.price+',00 €');
-        const divSettings = creaDiv(divContent, "cart__item__content__settings")
-        const settingsQuantity = creaDiv(divSettings, 'cart__item__content__settings__quantity')
-        creaParagraph(settingsQuantity , "Qté : ")
-        const inputQuantity = creaInput (settingsQuantity)
-        inputQuantity.type = ('number')
-        inputQuantity.classList.add('itemQuantity')
-        inputQuantity.name = ('itemQuantity')
-        inputQuantity.min = (1)
-        inputQuantity.max = (100)
-        inputQuantity.setAttribute('value', panier.quantity) 
-        const divDelete = creaDiv(divSettings, "cart__item__content__settings__delete")
+        const divSettings = creaDiv(divContent, "cart__item__content__settings");
+        const settingsQuantity = creaDiv(divSettings, 'cart__item__content__settings__quantity');
+        creaParagraph(settingsQuantity , "Qté : ");
+        const inputQuantity = creaInput (settingsQuantity);
+        inputQuantity.type = ('number');
+        inputQuantity.classList.add('itemQuantity');
+        inputQuantity.name = ('itemQuantity');
+        inputQuantity.min = (0);
+        inputQuantity.max = (100);
+        let pQuantity = inputQuantity.setAttribute('value', panier.quantity) ;
+        const divDelete = creaDiv(divSettings, "cart__item__content__settings__delete");
         const paraDelete = creaParagraph(divDelete, "Supprimer");
         paraDelete.classList.add('deleteItem');
-    })   
+        
+        
+        
+       
+        //               somme des canap 
+        
+        //console.log(somme) 
+        //let totalPrice = document.getElementById('totalPrice')
+        //console.log(produit.price)
+        
+        
+        paraDelete.addEventListener('click', function(){
+            console.log('cest clické')
+            console.log(cart.length)
+            if(panier.quantity>=1){
+                pQuantity = panier.quantity
+                pQuantity-1;
+                console.log(pQuantity)
+                return pQuantity
+                // cart[0].quantity-1
+                
+                
+            }
+        })
+    dsiplayTotal(document.getElementById('totalPrice'),panier.quantity, produit.price)
+        //for (let B of A ) {
+            
+        //}
+        //console.log(A.length)
+        return produit   
+    }) 
+    return panier  
 });
+
+
+
+function dsiplayTotal(target, quantity, price) {  
+    let totalPrixItem =quantity*price
+    somme+= totalPrixItem  
+    target.innerText = (somme+',00€')
+    console. log(somme+',00€')                
+}
+
+
 //            ***** fonction article *****
 function creaArticle (target, classe){
     let article = document.createElement('article')
