0f430e12f75a42178068d4a4d7f778e9477b824a
Effix <107648239+Effix1@users.noreply.github.com>
2023-01-15 19:23:29 +0100

    essaie mise en place de la fonction suprimé

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,14 +1,14 @@
 const cart = JSON.parse(localStorage.getItem('caddy'))||[] 
-let somme = 0
 cart.forEach(panier => {
     fetch('http://localhost:3000/api/products/'+panier.id)
     .then (function(res){
         if(res.ok){
             return res.json();
         }
-
+        
     })
     .then (function(produit){
+     
         const sectionItem = document.getElementById('cart__items');
         const article = creaArticle(sectionItem, "cart__item");
         const divImage = creaDiv(article, "cart__item__img"); 
@@ -31,48 +31,32 @@ cart.forEach(panier => {
         const divDelete = creaDiv(divSettings, "cart__item__content__settings__delete");
         const paraDelete = creaParagraph(divDelete, "Supprimer");
         paraDelete.classList.add('deleteItem');
-        
-        
-        
-       
-        //               somme des canap 
-        
-        //console.log(somme) 
-        //let totalPrice = document.getElementById('totalPrice')
-        //console.log(produit.price)
-        
-        
+     
+        const caddy = JSON.parse (localStorage.getItem('caddy'))|| []
+        console.log(caddy)
+
+        console.log(panier.quantity)
         paraDelete.addEventListener('click', function(){
             console.log('cest clické')
-            console.log(cart.length)
             if(panier.quantity>=1){
-                pQuantity = panier.quantity
-                pQuantity-1;
-                console.log(pQuantity)
-                return pQuantity
-                // cart[0].quantity-1
-                
-                
+               panier.quantity-1
+               console.log(panier.quantity)
+
+               return panier.quantity
             }
         })
-    dsiplayTotal(document.getElementById('totalPrice'),panier.quantity, produit.price)
-        //for (let B of A ) {
-            
-        //}
-        //console.log(A.length)
-        return produit   
+        
+        const totalQuantity = document.querySelector('#totalQuantity')
+
+         
+    dsiplayTotalPrice(document.getElementById('totalPrice'),panier.quantity, produit.price)
+    dsiplayTotalQuantity(document.getElementById('totalQuantity'),panier.quantity)
+    console.log(panier.quantity)
+
     }) 
-    return panier  
+    
 });
 
-
-
-function dsiplayTotal(target, quantity, price) {  
-    let totalPrixItem =quantity*price
-    somme+= totalPrixItem  
-    target.innerText = (somme+',00€')
-    console. log(somme+',00€')                
-}
 
 
 //            ***** fonction article *****
@@ -89,19 +73,21 @@ function creaDiv (target, classe){
     div.classList.add(classe)
     return div
 }
-
+//             ***** fonction création image *****
 function creaImg (target, source){
     let image = document.createElement('img')
     target.appendChild(image)
     image.setAttribute('src', source)
 }
 
+//             ***** fonction création titre(H2) *****
 function creaH2 (target, texte) {
     let h2= document.createElement('h2')
     let titre = target.appendChild(h2)
     titre.innerText = (texte)
 }
 
+//             ***** fonction création balise paragraphe *****
 function creaParagraph (target, texte) {
     let paragraphe= document.createElement('p')
     let para = target.appendChild(paragraphe)
@@ -109,8 +95,33 @@ function creaParagraph (target, texte) {
     return paragraphe
 }
 
+//             ***** fonction création balise input *****
 function creaInput (target){
     let input = document.createElement('input')
     target.appendChild(input)
     return input
 }
+
+//                ***** fonction display prix total *****
+
+let somme = 0
+function dsiplayTotalPrice(target, quantity, price) {  
+    let totalPrixItem =quantity*price
+    somme+= totalPrixItem  
+    target.innerText = (somme+',00 ')
+                  
+}
+
+//               ***** fonction display quantité *****
+
+let nombre = 0 
+function dsiplayTotalQuantity(target, quantity) {  
+    nombre+= quantity  
+    target.innerText = (nombre)             
+}
+
+//              ***** cretion fonction getpluriel *****
+function getpluriel (nb) {
+    return nb>1 ? "s" : "";
+}
+
--- front/js/product.js
+++ front/js/product.js
@@ -9,9 +9,6 @@ fetch("http://localhost:3000/api/products/"+ id)
 })    
 .then(function(produit){
 
-    const nom = (produit.name)
-    //const imageProduit = (produit.imageUrl)
-    //const textAlt = (produit.imageUrl)
     const prixUnitaire= (produit.price)
     const description = document.getElementById('description')
     const title = document.getElementById('title')
